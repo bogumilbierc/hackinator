e2c35efa7309323cfb3bda36d84cf263cd4715a9
Vladyslav Stashuk <slavik.st89@gmail.com>
2016-03-04 13:05:06 +0100

    Add Akinator api

---
+++ src/main/java/hackinator/api/IJavinator.java
@@ -0,0 +1,14 @@
+package hackinator.api;
+
+/**
+ * Created by kazak on 3/2/2016.
+ */
+public interface IJavinator {
+    Integer startSession();
+    String sendAnswer(String answer);
+    Integer endSession();
+    String[] getAllGuesses();
+    Boolean haveGuess();
+    Integer getStep();
+    String getCurrentQuestion();
+}
