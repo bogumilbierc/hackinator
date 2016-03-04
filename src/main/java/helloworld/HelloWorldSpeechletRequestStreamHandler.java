89ed6a5c7657adf8a778c2baa9d88ceb8e0c2bb4
Vladyslav Stashuk <slavik.st89@gmail.com>
2016-03-04 12:46:41 +0100

    Set up app id for hello world voice app

--- src/main/java/helloworld/HelloWorldSpeechletRequestStreamHandler.java
+++ src/main/java/helloworld/HelloWorldSpeechletRequestStreamHandler.java
@@ -28,7 +28,7 @@ public final class HelloWorldSpeechletRequestStreamHandler extends SpeechletRequ
          * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
          * Alexa Skill and put the relevant Application Ids in this Set.
          */
-        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
+        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.ef15f0aa-d0f8-475e-ab64-9bcc36d65452");
     }
 
     public HelloWorldSpeechletRequestStreamHandler() {
